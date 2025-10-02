6e009484d0a8a19f2023325d3655585163b3ee48
Abraham <aem19@sfu.ca>
2025-10-01 23:10:31 -0700

    Info for web

---
+++ Do_thing.js
@@ -0,0 +1,11 @@
+function alertMe() {
+    window.alert("Hi");
+}
+function save_name() {
+    const name = document.getElementById("gmail").value;
+    console.log(name);
+}
+function save_password() {
+    const password = document.querySelector(".password").value;
+    console.log(password);
+}
