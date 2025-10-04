973b907b806c2c0d0cb4de55f49f2b43b752d0dc
Abraham <aem19@sfu.ca>
2025-10-03 19:39:31 -0700

    Info for web

--- Do_thing.js
+++ Do_thing.js
@@ -6,6 +6,25 @@ function save_name() {
     console.log(name);
 }
 function save_password() {
-    const password = document.querySelector(".password").value;
+    const password = document.querySelector(".password1").value;
     console.log(password);
+}
+
+function addparagraph() {
+    const random = Math.floor(Math.random() * 5);
+    const number = "The random number is " + random;
+    console.log(number);
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = number;
+    document.body.appendChild(paragraph);
+}
+
+function save_text() {
+    const text = document.getElementById("savetext").value;
+    const show = "The text you wrote is: " + text;
+
+    const show_text = document.createElement("text");
+    show_text.textContent = show;
+    document.body.appendChild(show_text);
 }
